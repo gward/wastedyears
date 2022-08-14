cc94c150ed3619df596ffff1b47484710634b858
Greg Ward <greg@gerg.ca>
2022-08-13 21:17:41 -0400

    read config from a file (with configparser)

--- wastedyears/config.py
+++ wastedyears/config.py
@@ -1,14 +1,46 @@
 '''wastedyears configuration data'''
 
 from __future__ import annotations
+
+import configparser
+import dataclasses
 import os
 
 
 def get_config() -> Config:
     '''return a Config object ready to use'''
-    return Config()
+    defaults = get_default_config()
+    parser = configparser.ConfigParser()
+
+    config_home = _xdg_dir('XDG_CONFIG_HOME', '.config')
+
+    parser.read_string(defaults)
+    parser.read(os.path.join(config_home, 'wastedyears.cfg'))
+
+    return Config(
+        data_dir=parser.get('core', 'data_dir'),
+        db_url=parser.get('core', 'db_url'),
+    )
 
 
+def get_default_config() -> str:
+    '''return a hardcoded default config'''
+    # from https://specifications.freedesktop.org/basedir-spec/basedir-spec-0.6.html
+    data_home = _xdg_dir('XDG_DATA_HOME', '.local', 'share')
+
+    return f'''[core]
+data_dir={os.path.join(data_home, 'wastedyears')}
+db_url=sqlite:///%(data_dir)s/wastedyears.sqlite
+'''
+
+
+def _xdg_dir(env_var, *default_path):
+    return os.environ.get(
+        env_var,
+        os.path.join(os.environ['HOME'], *default_path))
+
+
+@dataclasses.dataclass
 class Config:
     '''An object that encapsulates all config settings for wastedyears.
 
@@ -16,15 +48,6 @@ class Config:
     '''
     data_dir: str
     db_url: str
-
-    def __init__(self):
-        # from https://specifications.freedesktop.org/basedir-spec/basedir-spec-0.6.html
-        data_home = os.environ.get(
-            'XDG_DATA_HOME',
-            os.path.join(os.environ['HOME'], '.local', 'share'))
-
-        self.data_dir = os.path.join(data_home, 'wastedyears')
-        self.db_url = 'sqlite:///' + os.path.join(self.data_dir, 'wastedyears.sqlite3')
 
     def create_data_dir(self):
         if not os.path.isdir(self.data_dir):
